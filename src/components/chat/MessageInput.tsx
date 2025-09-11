d197394cd803b1a9d43f7c341ee754317ee64720
ziauddin128 <ziauddindev2000@gmail.com>
2025-09-11 14:34:06 +0600

    on click msg approach 15

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -55,7 +55,8 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage }) => {
   return (
     // max-w-xl lg:max-w-[30rem] xl:max-w-3xl w-full
     <div>
-      <div className="p-[14px] bg-[#f0f2f5] fixed bottom-0  border-t border-gray-200 w-full mx-auto max-w-3xl md:max-w-[35rem] xl:max-w-[47rem] ">
+      {/* fixed bottom-0   */}
+      <div className="p-[14px] bg-[#f0f2f5] border-t border-gray-200 w-full mx-auto max-w-3xl md:max-w-[35rem] xl:max-w-[47rem] ">
         <form onSubmit={handleSendMessage} className="flex gap-2">
           <Input
             placeholder="Type a message..."
--- src/components/chat/MessagesContainer.tsx
+++ src/components/chat/MessagesContainer.tsx
@@ -72,8 +72,7 @@ const MessagesContainer: React.FC<MessagesContainerProps> = ({
   });
 
   return (
-    // flex-1
-    <ScrollArea className="w-full pb-4 bg-gray-100">
+    <ScrollArea className="flex-1 pb-4 bg-gray-100">
       <div className="px-4 pb-3 pt-[10px] h-full w-full overflow-y-auto  flex flex-col-reverse [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
         {dateKeys.map((dateKey) => {
           // const dateMessages = groupedMessages[dateKey];
