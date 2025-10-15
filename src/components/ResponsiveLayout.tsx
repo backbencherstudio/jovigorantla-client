dc91b416db54db6f12842b35a4a2809b190747a4
ziauddin128 <ziauddindev2000@gmail.com>
2025-10-15 11:22:31 +0600

    all-pages-title-top-top-the-childreen

--- src/components/ResponsiveLayout.tsx
+++ src/components/ResponsiveLayout.tsx
@@ -350,6 +350,24 @@ const ResponsiveLayout: React.FC<ResponsiveLayoutProps> = ({
               {/* Page Content */}
               {/* flex-1 bg-white min-h-screen pt-[120px] md:mx-2 */}
               <div className="bg-white h-[calc(100vh-65px)] mt-[65px] overflow-y-auto md:mx-2 ">
+                <div className="z-20 sticky top-[0px] bg-white border-b border-gray-100 px-3 py-2 flex items-center ">
+                  {!hideBackButton && (
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      onClick={handleBack}
+                      className=""
+                    >
+                      <ArrowLeft className="h-5 w-5" />
+                    </Button>
+                  )}
+                  <h1
+                    className={`text-xl font-bold sm:font-medium text-center w-full flex-1  mr-10 `}
+                  >
+                    {title}
+                  </h1>
+                </div>
+
                 {children}
               </div>
             </main>
