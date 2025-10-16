e7ecc4a521584cac406d0cce13634408ae3897e3
ziauddin128 <ziauddindev2000@gmail.com>
2025-10-16 12:23:43 +0600

    profile-remove-commented-code

--- src/pages/ListingDetailPage.tsx
+++ src/pages/ListingDetailPage.tsx
@@ -247,7 +247,7 @@ const ListingDetailPage = ({ openModal }) => {
 
   return (
     <>
-      <div className=" bg-white min-h-[calc(100vh-110px)]">
+      <div className="bg-white min-h-[calc(100vh-110px)]">
         <div
           className={`${
             isMobile && user?.id !== listing?.user_id && listing && "pb-24"
